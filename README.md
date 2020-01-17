6542f07d670fe405acf8af2d67518a6037face2f
aleksandryakunichev <yakunichev.aleksandr@gmail.com>
2020-01-17 18:19:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# electro-hydra
+
+Electron wrapper for HYDRA to support beautifull fullscreen code backgrounds
+
+![example](/.media/01.png?raw=true 'App usage example')
---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+// Allow insecure HTTPS on localhost
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+app.commandLine.appendSwitch('allow-insecure-localhost', 'true');
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    backgroundColor: '#000',
+    frame: false,
+    webPreferences: {},
+    simpleFullscreen: true,
+    fullscreen: true,
+  });
+
+  mainWindow.loadURL('https://localhost:8000');
+
+  mainWindow.on('closed', () => (mainWindow = null));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () =>
+  process.platform !== 'darwin' ? app.quit() : null,
+);
+
+app.on('activate', () => (mainWindow === null ? createWindow() : null));
